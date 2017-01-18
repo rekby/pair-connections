152f445fd49b512da5a160b6e4e5a7ec9ccb463a
Rekby <timofey@koolin.ru>
2017-01-18 11:01:48 +0300

    init

---
+++ stub-conn_test.go
@@ -0,0 +1,26 @@
+package pairconnections
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestCreateTwoTCPConnections(t *testing.T) {
+	c1, c2 := CreateTCPPairConnections()
+	defer c1.Close()
+	defer c2.Close()
+
+	go func() {
+		c1.Write([]byte("asd"))
+		c1.Write([]byte("aaa"))
+		c1.Write([]byte("555"))
+		c1.Close()
+	}()
+	buf, err := ioutil.ReadAll(c2)
+	if err != nil {
+		panic(err)
+	}
+	if string(buf) != "asdaaa555" {
+		t.Errorf("%s", buf)
+	}
+}
